20ad87a02e5d7b45dba3b5a6e0f74cd166ce6eb6
birdracoon <vogl@outlook.com>
2020-12-29 10:32:51 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'voggue-iobroker-vis adapter settings': {
+        'en': 'Adapter settings for voggue-iobroker-vis',
+        'de': 'Adaptereinstellungen für voggue-iobroker-vis',
+        'ru': 'Настройки адаптера для voggue-iobroker-vis',
+        'pt': 'Configurações do adaptador para voggue-iobroker-vis',
+        'nl': 'Adapterinstellingen voor voggue-iobroker-vis',
+        'fr': "Paramètres d'adaptateur pour voggue-iobroker-vis",
+        'it': "Impostazioni dell'adattatore per voggue-iobroker-vis",
+        'es': 'Ajustes del adaptador para voggue-iobroker-vis',
+        'pl': 'Ustawienia adaptera dla voggue-iobroker-vis',
+        'zh-cn': 'voggue-iobroker-vis的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
